8692d59eeaf383fc516838a80a16097d21101888
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-17 22:18:56 -0300

    feat: :lipstick: Finalização da página Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,6 +69,18 @@ export default function Home() {
           </span>
         </footer>
       </div>
+
+      {/* Right */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="max-w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="#" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
