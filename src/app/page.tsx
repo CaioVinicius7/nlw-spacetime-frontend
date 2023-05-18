a90cb64af35882ea324515730a7ee2d73fb803b7
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-18 20:54:10 -0300

    feat: :lipstick: Componente EmptyMemories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Copyright } from "@root/components/Copyright";
+import { EmptyMemories } from "@root/components/EmptyMemories";
 import { Hero } from "@root/components/Hero";
 import { SignIn } from "@root/components/SignIn";
 
@@ -22,14 +23,7 @@ export default function Home() {
 
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="max-w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{" "}
-            <a href="#" className="underline hover:text-gray-50">
-              criar agora!
-            </a>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   );
