69edebaa7233b8a62eac962eaa46c9a9821ef387
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-19 12:56:46 -0300

    feat: :passport_control: Api route de cadastro/autenticação

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { api } from "@root/lib/api";
+
+interface RegisterResponseBody {
+  token: string;
+}
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+
+  const code = searchParams.get("code");
+
+  const registerResponse = await api.post<RegisterResponseBody>("/register", {
+    code
+  });
+
+  const { token } = registerResponse.data;
+
+  const redirectURL = new URL("/", request.url);
+
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30; // 1 month
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      "Set-Cookie": `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`
+    }
+  });
+}
