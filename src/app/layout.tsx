8b94fe2a0400e5375a362bac0d542efffd30c739
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-20 02:30:00 -0300

    feat: :sparkles: Definindo root layout da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,14 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamJuree
 } from "next/font/google";
+import { cookies } from "next/headers";
+
+import { Blur } from "@root/components/Blur";
+import { Stripes } from "@root/components/Stripes";
+import { Profile } from "@root/components/Profile";
+import { SignIn } from "@root/components/SignIn";
+import { Hero } from "@root/components/Hero";
+import { Copyright } from "@root/components/Copyright";
 
 import "./globals.css";
 
@@ -25,12 +33,32 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has("token");
+
   return (
     <html lang="pt-BR">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-slate-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16 ">
+            <Blur />
+
+            <Stripes />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
