9bee9d58dd984fc4a24e59898086db7ddd5d65ca
Caio Vinícius <caio1525pereira@gmail.com>
2023-05-23 21:42:26 -0300

    feat: :sparkles: Listagem de memórias

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -5,7 +5,7 @@ import { NewMemoryForm } from "@root/components/NewMemoryForm";
 
 export default async function NewMemory() {
   return (
-    <section className="flex flex-1 flex-col gap-4">
+    <section className="flex flex-1 flex-col gap-4 p-16">
       <Link
         href="/"
         className="flex w-fit items-center gap-1  text-sm text-gray-200 hover:text-gray-100"
